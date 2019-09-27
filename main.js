22c03b71d36431bff425650fdeb4075321d4261b
CrazyTim <impdeveloper@gmail.com>
2019-09-28 00:50:04 +1000

    Refactor code that processes tags and links

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 function initalise() {
 
-	var el_tags = document.getElementsByClassName('tags');
-	for (i = 0; i < el_tags.length; ++i) {
-		var html_tags = '';
-		var words = el_tags[i].innerHTML.split(",");
-		for (j = 0; j < words.length; ++j) {
-			if (words[j].trim() == '') { continue; }
-			html_tags += '<span class="tag">' + words[j].trim().toLowerCase() + '</span>';
-		}
-	    el_tags[i].innerHTML = html_tags;
-	}
-
-	// make all links open in new tab
-	for (var c = document.getElementsByTagName("a"), a = 0;a < c.length;a++) { 
-		var b = c[a]; 
-		b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank") 
-	}
+  // create tags dynamically
+  document.querySelectorAll('.tags').forEach( (e) => {
+    let html = '';
+    e.innerHTML.split(",").forEach( (w) => {
+      if (w.trim() === '') { return; }
+      html += '<span class="tag">' + w.trim().toLowerCase() + '</span>';
+    });
+    e.innerHTML = html;
+  });
+ 
+  // ensure all links open in a new tab
+  document.querySelectorAll("a").forEach( (e) => {
+    e.getAttribute("href") && e.hostname !== location.hostname && (e.target = "_blank");
+  });
 
 }

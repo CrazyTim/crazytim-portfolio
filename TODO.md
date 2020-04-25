053b340f865a78fc2e58d0b7b44ac6e11ffe8deb
CrazyTim <impdeveloper@gmail.com>
2020-04-25 20:26:16 +1000

    Add tag search box

--- TODO.md
+++ TODO.md
@@ -1,9 +1,7 @@
 ### TODO:
-- only show the first couple of important tags, the remainder hidden by "plus 5 more..." label, so as not to overwhelm.
-- simple tag filter
+- Make a simple blog
 
 ##### Notes:
 - example: http://www.grokcode.com/programmer-portfolio/
-- example: https://lesterchan.net/portfolio/programming/
 - ideas: http://www.grokcode.com/58/the-power-of-a-programming-portfolio/
 - ideas: https://www.thesoftwareguild.com/blog/programming-portfolio/
--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+let items = [];
+
 function initalise() {
+
+  items = document.querySelectorAll('.item');
 
   // create tags dynamically
   document.querySelectorAll('.tags').forEach( (e) => {
@@ -15,4 +19,69 @@ function initalise() {
     e.getAttribute("href") && e.hostname !== location.hostname && (e.target = "_blank");
   });
 
+  const filterWrapper = document.querySelector('.filter');
+  const filter = document.querySelector('.filter input');
+  const btnClear = document.querySelector('.filter .btn-clear');
+
+  filter.oninput = () => {
+    search(filter.value.toLowerCase());
+  };
+
+  btnClear.onclick = () => {
+    filter.value = '';
+    search('');
+  };
+
+}
+
+function search(searchText) {
+
+  const filterWrapper = document.querySelector('.filter');
+  const found = findItemsWithTag(searchText);
+
+  for (let i=0; i<items.length; i++) {
+    const item = items[i];
+    if (found.includes(item)) {
+      item.classList.remove('hidden');
+    } else {
+      item.classList.add('hidden');
+    }
+  }
+
+  const foundResult = document.querySelector('.filter-wrapper .result')
+
+  if (found.length == items.length) {
+    filterWrapper.classList.remove('found');
+    foundResult.classList.add('hidden');
+  } else {
+    filterWrapper.classList.add('found');
+    foundResult.classList.remove('hidden');
+
+    let m = 'Matches';
+    if (found.length == 1) { m = 'Match'; }
+    foundResult.textContent = found.length + ' ' + m;
+  }
+
+}
+
+// find items with matching tags
+function findItemsWithTag(s) {
+  const found = new Set();
+  for (let i=0; i<items.length; i++) {
+    const tags = items[i].querySelectorAll('.tag');
+    for (let j=0; j<tags.length; j++) {
+      if (tags[j].textContent.includes(s)) {
+        if (s !== '') {
+          tags[j].classList.add('found'); 
+        } else {
+          tags[j].classList.remove('found');
+        }
+        found.add(items[i]);
+      } else {
+        tags[j].classList.remove('found');
+      }
+
+    }
+  }
+  return Array.from(found);
 }
